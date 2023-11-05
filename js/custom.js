5240fdaa38362443bfb472b264c4e1c2b5556a0a
akshat26120 <26120akshtaravdiya@gmail.com>
2023-11-05 10:59:49 +0530

    ok

--- js/custom.js
+++ js/custom.js
@@ -1,63 +1,61 @@
+/*-------------------------------------------------------------------------------
+  PRE LOADER
+-------------------------------------------------------------------------------*/
 
-
-  /*-------------------------------------------------------------------------------
-    PRE LOADER
-  -------------------------------------------------------------------------------*/
-
-  $(window).load(function(){
+$(window).load(function() {
     $('.preloader').fadeOut(1000); // set duration in brackets    
-  });
+});
 
 
 
-  /* HTML document is loaded. DOM is ready. 
-  -------------------------------------------*/
+/* HTML document is loaded. DOM is ready. 
+-------------------------------------------*/
 
-  $(document).ready(function() {
+$(document).ready(function() {
 
 
-  /*-------------------------------------------------------------------------------
-    Navigation - Hide mobile menu after clicking on a link
-  -------------------------------------------------------------------------------*/
+    /*-------------------------------------------------------------------------------
+      Navigation - Hide mobile menu after clicking on a link
+    -------------------------------------------------------------------------------*/
 
-    $('.navbar-collapse a').click(function(){
+    $('.navbar-collapse a').click(function() {
         $(".navbar-collapse").collapse('hide');
     });
 
 
     $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-  });
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
 
 
 
-  /*-------------------------------------------------------------------------------
-    jQuery Parallax
-  -------------------------------------------------------------------------------*/
+    /*-------------------------------------------------------------------------------
+      jQuery Parallax
+    -------------------------------------------------------------------------------*/
 
     function initParallax() {
-    $('#home').parallax("100%", 0.1);
-    $('#about').parallax("100%", 0.3);
-    $('#service').parallax("100%", 0.2);
-    $('#experience').parallax("100%", 0.3);
-    $('#education').parallax("100%", 0.1);
-    $('#quotes').parallax("100%", 0.3);
-    $('#contact').parallax("100%", 0.1);
-    $('footer').parallax("100%", 0.2);
+        $('#home').parallax("100%", 0.1);
+        $('#about').parallax("100%", 0.3);
+        $('#service').parallax("100%", 0.2);
+        $('#experience').parallax("100%", 0.3);
+        $('#education').parallax("100%", 0.1);
+        $('#quotes').parallax("100%", 0.3);
+        $('#contact').parallax("100%", 0.1);
+        $('footer').parallax("100%", 0.2);
 
-  }
-  initParallax();
+    }
+    initParallax();
 
 
 
-  /*-------------------------------------------------------------------------------
-    smoothScroll js
-  -------------------------------------------------------------------------------*/
-  
+    /*-------------------------------------------------------------------------------
+      smoothScroll js
+    -------------------------------------------------------------------------------*/
+
     $(function() {
         $('.custom-navbar a, #home a').bind('click', function(event) {
             var $anchor = $(this);
@@ -67,15 +65,25 @@
             event.preventDefault();
         });
     });
-  
 
 
-  /*-------------------------------------------------------------------------------
-    wow js - Animation js
-  -------------------------------------------------------------------------------*/
 
-  new WOW({ mobile: false }).init();
+    /*-------------------------------------------------------------------------------
+      wow js - Animation js
+    -------------------------------------------------------------------------------*/
+
+    new WOW({ mobile: false }).init();
 
 
-  });
+});
 
+window.addEventListener('load', function() {
+    const preloader = document.querySelector('.preloader');
+    const content = document.querySelector('.content');
+
+    // Hide the preloader and show the content after the page is fully loaded
+    setTimeout(function() {
+        preloader.style.display = 'none';
+        content.style.display = 'block';
+    }, 2000); // Change the time (in milliseconds) as needed for your preloader
+});
